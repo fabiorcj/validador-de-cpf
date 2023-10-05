a6562ec2b0b21a0d9eb0c404919de539cfb04df1
Fabiorcj <fabio_iff@live.com>
2023-10-05 08:57:40 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+const btn = document.querySelector("#send");
+
+btn.addEventListener("click", function(e) {
+    e.preventDefault();
+    const cpf1 = document.querySelector("#cpf");
+    const cpfEnviado = cpf1.value;
+    
+    function ValidaCPF(cpfEnviado) {
+        Object.defineProperty(this, 'cpfLimpo' ,  {
+            enumerable: true,
+            get: function() {
+                return cpfEnviado.replace(/\D+/g , '');
+            }
+        });
+    }
+
+    ValidaCPF.prototype.valida = function() {
+        if(typeof this.cpfLimpo === 'undefined') return false;
+        if( this.cpfLimpo.length !== 11) return false;
+        if(this.isSequencia()) return false;
+
+        const cpfParcial = this.cpfLimpo.slice(0, -2);
+        const digito1 = this.criaDigito(cpfParcial);
+        const digito2 = this.criaDigito(cpfParcial + digito1);
+
+        const novoCpf = cpfParcial + digito1 + digito2;
+        return novoCpf === this.cpfLimpo;
+    };
+
+    ValidaCPF.prototype.criaDigito= function(cpfParcial) {
+        const cpfAraay = Array.from(cpfParcial);
+
+        let regressivo = cpfAraay.length + 1;
+        const total = cpfAraay.reduce((ac, val) => {
+            ac += (regressivo * Number(val));
+            regressivo --;
+            return ac
+        }, 0);
+
+        const digito = 11 - (total % 11);
+        return digito > 9 ? '0' : String(digito);
+    };
+
+    ValidaCPF.prototype.isSequencia = function() {
+        const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+        return sequencia === this.cpfLimpo;
+    };
+
+    const cpf = new ValidaCPF(cpfEnviado);
+
+ if(cpf.valida()) {
+    alert('Cpf válido');
+ } else {
+    alert('Cpf inválido');
+ }
+
+});
+
+
+
+
+
+
+
